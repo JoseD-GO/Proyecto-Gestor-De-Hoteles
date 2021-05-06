129313e989ce95fbcbc2f9ba875f5e9943dad1c4
José Góngora <jgongora-2019063@kinal.edu.gt>
2021-05-05 21:11:01 -0600

    Se agregaron las rutas nuevas

--- Gestor_de_Hoteles/app.js
+++ Gestor_de_Hoteles/app.js
@@ -9,6 +9,8 @@ const userController = require('./src/controllers/user.controller')
 
 const user_routes = require('./src/routes/user.routes')
 const hotel_routes = require('./src/routes/hotel.routes')
+const type_routes = require('./src/routes/eventtype.routes')
+const event_routes = require('./src/routes/event.routes')
 
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
@@ -17,6 +19,8 @@ app.use(cors())
 
 app.use('/api', user_routes)
 app.use('/api', hotel_routes)
+app.use('/api', type_routes)
+app.use('/api', event_routes)
 
 userController.createAdmin()
 
